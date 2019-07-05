fc1da8a44de2c11a5772b6113803a47d0d4a50e4
Albert Jian <albertokjian@gmail.com>
2019-07-05 02:14:11 -0700

    initial commit

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "New York",
+        "excerpt":"New York has food. New York good.      Nintendo NY  Back to top ↑   Rockerfeller Center  Back to top ↑  ","categories": [],
+        "tags": [],
+        "url": "/travel/new-york/",
+        "teaser":null},{
+        "title": "Japan",
+        "excerpt":"{% google_map src=\"_data/japan.yml\" zoom=\"12\" %}  # Summary  ## Tokyo Tower {{ site.data.ui-text[site.locale].back_to_top | default: 'Back to Top' }} &uarr; ","categories": [],
+        "tags": [],
+        "url": "/travel/japan/",
+        "teaser":null}]
