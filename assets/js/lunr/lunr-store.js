d259ee108fb6f7f93da83a367821414bc6eee80a
Albert Jian <albertokjian@gmail.com>
2019-07-06 01:26:12 -0700

    fixed map on mobile

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,10 +2,10 @@ var store = [{
         "title": "New York",
         "excerpt":"New York has food. New York good.      Nintendo NY  Back to top ↑   Rockerfeller Center  Back to top ↑  ","categories": [],
         "tags": [],
-        "url": "/travel/new-york/",
+        "url": "https://albertokjian.github.io/travel/new-york/",
         "teaser":null},{
         "title": "Japan",
         "excerpt":"   Summary   Tokyo Tower  Back to top ↑  ","categories": [],
         "tags": [],
-        "url": "/travel/japan/",
+        "url": "https://albertokjian.github.io/travel/japan/",
         "teaser":null}]
